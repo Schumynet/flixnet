97b8bdf87647054b2e549fcf698bdf87386dfd8e
Schumynet <netprime311@gmail.com>
2025-09-01 10:13:00 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ js
 /* ------------------------------------------------------------------ */
 /*  TMDB configuration – inserisci la tua chiave API qui               */
 /* ------------------------------------------------------------------ */
-export const TMDB_API_KEY = "YOUR_TMDB_API_KEY";   // <-- SOSTITUISCI
+export const TMDB_API_KEY = "be78689897669066bef6906e501b0e10";   // <-- SOSTITUISCI
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 export const TMDB_IMG_BASE = "https://image.tmdb.org/t/p/w500";
 export const TMDB_IMG_ORIGINAL = "https://image.tmdb.org/t/p/original";
